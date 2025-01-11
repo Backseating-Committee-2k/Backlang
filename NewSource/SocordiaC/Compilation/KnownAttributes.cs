0fbd66a32b37b6c97a03527b222c90f14d062cd0
Chris Anders <filmee24@gmail.com>
2025-01-11 23:05:44 +0100

    fix: KnownAttributes

--- NewSource/SocordiaC/Compilation/KnownAttributes.cs
+++ NewSource/SocordiaC/Compilation/KnownAttributes.cs
@@ -5,34 +5,16 @@ using Socordia.Core.CompilerService;
 
 namespace SocordiaC.Compilation;
 
-public class KnownAttributes
+public class KnownAttributes(ModuleResolver resolver)
 {
-    private readonly Dictionary<Type, CustomAttrib> _cache = new();
-    private readonly ModuleResolver _resolver;
-
-    public KnownAttributes(ModuleResolver resolver)
-    {
-        _resolver = resolver;
-
-        GetAttribute<ReadOnlyAttribute>();
-        GetAttribute<UnitAttribute>();
-        GetAttribute<ExtensionAttribute>();
-    }
-
-    public CustomAttrib GetAttribute<T>()
+    public CustomAttrib GetAttribute<T>(params object[] args)
         where T : Attribute
     {
         var type = typeof(T);
-        if (_cache.TryGetValue(type, out var cachedAttrib))
-        {
-            return cachedAttrib;
-        }
-
-        var importedType = _resolver.Import(type);
+        var importedType = resolver.Import(type);
         var ctor = importedType.FindMethod(".ctor");
-        var customAttrib = new CustomAttrib(ctor);
+        var customAttrib = new CustomAttrib(ctor, [.. args]);
 
-        _cache[type] = customAttrib;
         return customAttrib;
     }
 }
