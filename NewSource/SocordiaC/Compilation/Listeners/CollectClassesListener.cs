7c169935f7e3ea56dadf9888298e4589b81f35e3
Chris Anders <filmee24@gmail.com>
2025-01-25 14:36:27 +0100

    feat: Add base type resolving

--- NewSource/SocordiaC/Compilation/Listeners/CollectClassesListener.cs
+++ NewSource/SocordiaC/Compilation/Listeners/CollectClassesListener.cs
@@ -10,19 +10,11 @@ public class CollectClassesListener : Listener<Driver, AstNode, ClassDeclaration
     protected override void ListenToNode(Driver context, ClassDeclaration node)
     {
         var ns = context.GetNamespaceOf(node);
-        var type = context.Compilation.Module.CreateType(ns, node.Name,
-            Utils.GetTypeModifiers(node), (TypeDefOrSpec)GetBaseType(node, context.Compilation));
+        var type = context.Compilation.Module.CreateType(ns, node.Name, Utils.GetTypeModifiers(node));
 
         Mappings.Types[node] = type;
 
         Utils.EmitAnnotations(node, type);
-    }
-
-    private TypeDesc? GetBaseType(ClassDeclaration node, DistIL.Compilation compilation)
-    {
-        if (node.BaseType == null) return compilation.Module.Resolver.Import(typeof(object));
-
-        return Utils.GetTypeFromNode(node.BaseType, compilation.Module);
     }
 
     protected override bool ShouldListenToChildren(Driver context, AstNode node)
