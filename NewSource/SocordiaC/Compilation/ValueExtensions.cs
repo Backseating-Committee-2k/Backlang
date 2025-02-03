b80ef93135aeff6e6366be23a150b30c0451d2d7
Chris Anders <filmee24@gmail.com>
2025-02-03 21:18:54 +0100

    refactor: Cleanup

--- NewSource/SocordiaC/Compilation/ValueExtensions.cs
+++ NewSource/SocordiaC/Compilation/ValueExtensions.cs
@@ -1,13 +1,15 @@
+using DistIL.IR;
+using Socordia.CodeAnalysis.AST;
+
 namespace Socordia.Compilation;
 
 public static class ValueExtensions
 {
     public static void EnsureType<T>(this Value value, AstNode node)
     {
-        //ToDo: fix this
-        if (value.Type != typeof(T))
+        if (value.ResultType.ToString() != typeof(T).FullName)
         {
-            node.AddError($"Expected value of type {type}, but got {value.Type}");
+            node.AddError($"Expected value of type {typeof(T)}, but got {value.ResultType}");
         }
     }
 }
